a2dedcf983bd3e4c1f70e5fd47ff229f899e44a0
ARNAB DAS <raj713335@gmail.com>
2022-04-26 21:29:05 +0530

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,8 +2,17 @@
  * @type import('hardhat/config').HardhatUserConfig
  */
  
+const INFURA_URL = 'https://rinkeby.infura.io/v3/94dbac5b3ff04f9da1854e01ae406dd6';
+const PRIVATE_KEY = 'private_key';
+
 require('@nomiclabs/hardhat-waffle');
 
 module.exports = {
   solidity: "0.8.0",
+  networks: {
+	  rinkeby: {
+		  url: INFURA_URL,
+		  accounts: [`0x${PRIVATE_KEY}`]
+	  }
+  }
 };
